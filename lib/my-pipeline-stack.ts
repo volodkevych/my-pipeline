408a9510d1b06d4e7e334da9008372ded8a27792
Oksana Volodkevych <oksanaov@amazon.com>
2025-08-15 18:11:34 +0200

    reverted cdk diff, added app stack

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, ShellStep, ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,8 +13,14 @@ export class MyPipelineStack extends cdk.Stack {
         input: CodePipelineSource.gitHub('volodkevych/my-pipeline', 'main', {
           authentication: cdk.SecretValue.secretsManager('my-pipeline-github-token')
         }),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth', 'npx cdk diff']
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+
+    const appStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "681702200272", region: "eu-west-1" }
+    }));
+
+    appStage.addPost(new ManualApprovalStep('approval'));
   }
 }
